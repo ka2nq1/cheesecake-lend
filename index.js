acf18331c2c163e533d4ed9ad407725cf697611c
v.pastukh <v.pastukh@foodtech.team>
2024-02-06 10:54:38 +0200

    fixes

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-$(".slick-slider").slick({
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+const accordItem = document.querySelectorAll(".accordion-item");
+accordItem.forEach((e) => {
+  e.addEventListener("click", toggleItem, false);
 });
 
-$(document).ready(function() {
-    $(".questionsItem").click(function() {
-      var $currentItem = $(this);
-      var $otherItems = $(".questionsItem").not($currentItem);
-      var $secondText = $currentItem.find(".secondText");
+function toggleItem() {
+  const isActive = this.classList.contains("active");
 
-      if ($secondText.is(":hidden")) {
-        // Закриваємо інші блоки
-        $otherItems.find(".secondText").slideUp(300);
-        $currentItem.removeClass("active"); // Видаляємо клас active у інших блоків
-        $otherItems.css("max-height", "140px");
+  accordItem.forEach((e) => {
+    if (e.classList.contains("active")) {
+      e.classList.remove("active")
+    }
+  })
 
-        // Відкриваємо поточний блок
-        $secondText.slideDown(300);
-        $otherItems.removeClass("active");
-        $currentItem.addClass("active"); // Додаємо клас active до поточного блоку
-        $currentItem.css("max-height", "300px");
-      } else {
-        // При закриванні видаляємо клас active у всіх блоків
-        $currentItem.removeClass("active");
-        $otherItems.css("max-height", "140px");
-        $secondText.slideUp(300);
-      }
-    });
-  });
+  if (!isActive) {
+    this.classList.add("active")
+  }
+}
